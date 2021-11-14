a7fc5e05aa1ec9a844561e1571f08f22830f8ce7
Heitor Carneiro <heitorgcarneiro@gmail.com>
2021-11-14 11:18:48 -0300

    feat(init): using logger

--- singlemodule/application/web/actions/home.go
+++ singlemodule/application/web/actions/home.go
@@ -2,6 +2,7 @@ package actions
 
 import (
 	"fmt"
+	"github.com/gobuffalo/logger"
 	"github.com/gobuffalo/pop/v5"
 	"net/http"
 	"singlemodule/domain/user"
@@ -12,6 +13,15 @@ import (
 // HomeHandler is a default handler to serve up
 // a home page.
 func HomeHandler(c buffalo.Context) error {
+	// https://github.com/gobuffalo/buffalo/blob/28bd05f88648468281d76c439c36f8ff2482cf91/default_context_test.go#L20
+	// https://github.com/gobuffalo/buffalo/blob/28bd05f88648468281d76c439c36f8ff2482cf91/options.go#L135
+	log := logger.New(logger.DebugLevel).WithField("handler", "HomeHandler")
+	log.Info("Running handler...")
+
+	log = c.Logger().WithField("handler", "HomeHandler")
+	log.Info("Running handler with request_id...")
+
+	c.Logger().Info("Sava user in database")
 	model := &user.User{}
 
 	// Get the DB connection from the context
@@ -25,5 +35,6 @@ func HomeHandler(c buffalo.Context) error {
 		return err
 	}
 
+	c.Logger().Info("User user saved successfully")
 	return c.Render(http.StatusOK, r.JSON(model))
 }
